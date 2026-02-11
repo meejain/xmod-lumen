48c431215745d47563c48258d5819ba2f42f64bf
meejain <meetkjain83@gmail.com>
2026-02-11 08:27:21 +0530

    Enabling homepage

--- blocks/cards-stats/cards-stats.js
+++ blocks/cards-stats/cards-stats.js
@@ -1,21 +1,44 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-stats-card-image';
-      else div.className = 'cards-stats-card-body';
-    });
+    const cols = [...row.children];
+    if (cols.length < 1) return;
+
+    const imageCol = cols.find((c) => c.querySelector('picture'));
+    const bodyCol = cols.find((c) => !c.querySelector('picture'));
+    if (!bodyCol) return;
+
+    bodyCol.className = 'cards-stats-card-body';
+    const ctaEl = bodyCol.querySelector('a')?.closest('strong') || bodyCol.querySelector('a');
+
+    const imageWrap = document.createElement('div');
+    imageWrap.className = 'cards-stats-card-image-wrap';
+    if (imageCol) {
+      const imageDiv = imageCol.querySelector('picture')?.closest('div') || imageCol;
+      imageDiv.className = 'cards-stats-card-image';
+      imageWrap.append(imageDiv);
+      if (ctaEl) {
+        const ctaWrap = document.createElement('div');
+        ctaWrap.className = 'cards-stats-card-cta';
+        ctaWrap.append(ctaEl);
+        imageWrap.append(ctaWrap);
+      }
+    } else if (ctaEl) {
+      bodyCol.append(ctaEl);
+    }
+
+    li.append(bodyCol, imageWrap);
     ul.append(li);
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     img.closest('picture').replaceWith(optimizedPic);
   });
+
   block.textContent = '';
   block.append(ul);
 }
